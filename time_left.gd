25ee5d4612ee7d184ba774b8e867d43bddc6822f
Rac0lW <racolw@outlook.com>
2025-02-20 12:51:29 +0800

    Update: Changed some expression of the TimeLeft.

--- time_left.gd
+++ time_left.gd
@@ -10,16 +10,16 @@ func _on_timer_timeout() -> void:
 	match Settings.current_states:
 		Settings.States.Working:
 			if work_or_long_rest_timer.time_left > 60:
-				text = "Time remaining: %2.f mins" % (work_or_long_rest_timer.time_left / 60)
+				text = "%2.f mins Left" % (work_or_long_rest_timer.time_left / 60)
 			else:
-				text = "Time remaining: %2.f s" % (work_or_long_rest_timer.time_left)
+				text = "%2.f s Left" % (work_or_long_rest_timer.time_left)
 		Settings.States.Resting:
 			if rest_timer.time_left > 60:
-				text = "Time remaining: %2.f mins" % (rest_timer.time_left / 60)
+				text = "%2.f mins Left" % (rest_timer.time_left / 60)
 			else:
-				text = "Time remaining: %2.f s" % rest_timer.time_left 
+				text = "%2.f s Left" % rest_timer.time_left 
 		Settings.States.LongResting:
 			if work_or_long_rest_timer.time_left > 60:
-				text = "Time remaining: %2.f mins" % (work_or_long_rest_timer.time_left / 60)
+				text = "%2.f mins Left" % (work_or_long_rest_timer.time_left / 60)
 			else:
-				text = "Time remaining: %2.f s" % work_or_long_rest_timer.time_left 
+				text = "%2.f s Left" % work_or_long_rest_timer.time_left 
